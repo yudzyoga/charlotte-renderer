b230a5f424569f7a7a61d43ba9ed9cdfc4b15be8
yuse00001 <yuse00001@stud.uni-saarland.de>
2023-12-13 12:03:26 +0100

    update principled

--- src/bsdfs/roughconductor.cpp
+++ src/bsdfs/roughconductor.cpp
@@ -21,7 +21,17 @@ public:
         // extremely specular distributions (alpha values below 10^-3)
         const auto alpha = std::max(float(1e-3), sqr(m_roughness->scalar(uv)));
 
-        NOT_IMPLEMENTED
+        Color R = m_reflectance->evaluate(uv);
+        Vector wh = (wo + wi) / (wo + wi).length();
+        float D = lightwave::microfacet::evaluateGGX(alpha, wh);
+
+        float G_wi = lightwave::microfacet::smithG1(alpha, wh, wi);
+        float G_wo = lightwave::microfacet::smithG1(alpha, wh, wo);
+        
+        float cos_theta_i = wi.y() / sqrt(pow(wi.x(), 2) + pow(wi.z(), 2));
+        float cos_theta_o = wo.y() / sqrt(pow(wo.x(), 2) + pow(wo.z(), 2));
+        
+        return {.value=R*D*G_wi*G_wo/(4*cos_theta_o*cos_theta_i)};
 
         // hints:
         // * the microfacet normal can be computed from `wi' and `wo'
@@ -31,8 +41,37 @@ public:
                       Sampler &rng) const override {
         const auto alpha = std::max(float(1e-3), sqr(m_roughness->scalar(uv)));
 
-        NOT_IMPLEMENTED
+        // sample microfacet normal
+        Vector normal = lightwave::microfacet::sampleGGXVNDF(alpha, wo, rng.next2D());
         
+        // compute wi, can easily be computed by reflecting the ray around the sampled normal
+        Vector wi = lightwave::reflect(wo, normal);
+
+        // microfacet normal pdf
+        float pdf = lightwave::microfacet::pdfGGXVNDF(alpha, normal, wo);
+
+        // compute jacobian term
+        float probInput = pdf * lightwave::microfacet::detReflection(normal, wo);
+        // float theta = atan2(sqrt(pow(wi.x(), 2) + pow(wi.z(), 2)), wi.y());
+        // The weight of the sample, given by cos(theta) * B(wi, wo) / p(wi)
+        // Color weight = Color(cos(theta) * Frame(normal).bitangent / probInput);
+
+        float G_wi = lightwave::microfacet::smithG1(alpha, normal, wi);
+
+        Color weight = m_reflectance->evaluate(uv) * G_wi;
+
+        // return {
+        //     .wi     = wi,
+        //     .weight = Color(probInput),
+        // };
+
+
+        // float weight = InvPi/cosineHemispherePdf(wi)*Frame::cosTheta(wi);//always equal to one
+        return BsdfSample{
+            .wi=wi,
+            .weight=weight,
+            .pdf=pdf
+        };
         // hints:
         // * do not forget to cancel out as many terms from your equations as possible!
         //   (the resulting sample weight is only a product of two factors)
