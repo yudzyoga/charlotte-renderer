d1651139c888620fbe8d5669e915cc89b156c6b8
yuse00001 <yuse00001@stud.uni-saarland.de>
2023-12-17 18:58:46 +0100

    update two modes of the pathtracer

--- src/integrators/pathtracer.cpp
+++ src/integrators/pathtracer.cpp
@@ -17,67 +17,127 @@ public:
      * @brief The job of an integrator is to return a color for a ray produced by the camera model.
      * This will be run for each pixel of the image, potentially with multiple samples for each pixel.
      */
-    
-    Color Li(const Ray &ray, Sampler &rng) override {
-        assert(m_depth >= 2);
-        if(!m_scene->hasLights()) nee = false;
-        /*
-        incremental update:
-        prev_le <- prev_le + prev_weight * new_le
-        prev_weight <- prev_weight * new_weight
-        */
-        Color prev_weight = Color(1.f);
-        Color prev_le     = Color(0.f);
-        Color new_li      = Color(0.f);
-        Ray   bounceRay   = ray; //with depth ==0
 
-        while(bounceRay.depth < m_depth){
-            // update ray and its depth
-            // every event check light(nne)            
-            Intersection its = m_scene->intersect(bounceRay, rng);
-            if(!its) {
-                // only break when escape the scene
-                new_li = m_scene->evaluateBackground(bounceRay.direction).value;
-                break;
-            } 
-            else{
-                Color new_le      = Color(0.f);
-                Color new_weight  = Color(1.f);
+    Color RecurseRay(const Ray &ray, Sampler &rng){
+        Color light = Color(0.f);
+        // apply zeros instead of evaluating background to avoid
+        // adding some more values to the image
+        // if (ray.depth >= m_depth) return m_scene->evaluateBackground(ray.direction).value; 
+        if (ray.depth >= m_depth) return light; 
+        
+        // check whether the ray intersect
+        bool isIntersected = m_scene->intersect(ray, INFINITY, rng);
+        if (!isIntersected) {
+            // if no intersection, just evaluate the background
+            return m_scene->evaluateBackground(ray.direction).value; 
+        } else {
+            // check intersection
+            Intersection its = m_scene->intersect(ray, rng);
 
-                if(nee && bounceRay.depth < m_depth-1){
-                    // nne is recognized as a new bounce, for last bounce, nne is not considered
-                    LightSample light_sample = m_scene->sampleLight(rng);
+            // check any intersection with existing light sources
+            if(nee && ray.depth < m_depth-1){
+                // nne is recognized as a new bounce, for last bounce, nne is not considered
+                LightSample light_sample = m_scene->sampleLight(rng);
 
-                    if(!light_sample.light->canBeIntersected()) { 
-                        DirectLightSample direct_light_sample = light_sample.light->sampleDirect(its.position, rng);
-                        bool              light_its           = m_scene->intersect( Ray(its.position, direct_light_sample.wi).normalized(),direct_light_sample.distance,rng);
+                if(!light_sample.light->canBeIntersected()) { 
+                    DirectLightSample direct_light_sample = light_sample.light->sampleDirect(its.position, rng);
+                    bool light_its = m_scene->intersect( Ray(its.position, direct_light_sample.wi).normalized(),direct_light_sample.distance,rng);
 
-                        if(!light_its)
-                        {
-                            BsdfEval bsdf_eval                = its.evaluateBsdf(direct_light_sample.wi);
-                                     new_le                  += direct_light_sample.weight * bsdf_eval.value / light_sample.probability;
-                        }
+                    if(!light_its)
+                    {
+                        BsdfEval bsdf_eval = its.evaluateBsdf(direct_light_sample.wi);
+                        light += direct_light_sample.weight * bsdf_eval.value / light_sample.probability;
                     }
                 }
-
-                if(its.instance->emission()) new_le += its.evaluateEmission(); //continue tracing
-                
-                //sample next ray direction
-                auto sample_result = its.sampleBsdf(rng);
-                new_weight         = sample_result.weight;
-
-                //incremental update
-                prev_le           += (prev_weight * new_le);
-                prev_weight       *= new_weight;   
-
-                if(bounceRay.depth >= m_depth-1) break; //no need to update bounceRay, shortcut
-
-                //update bounceRay for next iteration, depth +1
-                bounceRay = Ray(its.position, sample_result.wi, bounceRay.depth+1).normalized();             
             }
+
+            //if the hitted obj emits light, assign its color to the pixel
+            if(its.instance->emission()) return light + its.evaluateEmission();
+
+            auto sample_result = its.sampleBsdf(rng);
+            Color weight = sample_result.weight;
+            if(weight==Color(0)) return light;
+
+            // generate the next ray, transmitted from the hitting surface.
+            // add the counting value of the new ray for termination
+            Ray nextRay = Ray(its.position, sample_result.wi, ray.depth+1).normalized();
+            
+            // do repeat the ray recursively
+            Color nextColor = RecurseRay(nextRay, rng);
+
+            // return the light and value of the next ray intersection
+            return light + (weight * nextColor);
         }
-        //initial values: prev_le=0, prev_weight=1, new_li=0
-        return prev_le + prev_weight * new_li;
+    }
+    
+    Color Li(const Ray &ray, Sampler &rng) override {
+        bool isRecurse = true;
+        if(!m_scene->hasLights()) nee = false;
+
+        if (isRecurse) {
+            Color outLight = RecurseRay(ray, rng);
+            return outLight;
+        } else {
+            assert(m_depth >= 2);
+            if(!m_scene->hasLights()) nee = false;
+            /*
+            incremental update:
+            prev_le <- prev_le + prev_weight * new_le
+            prev_weight <- prev_weight * new_weight
+            */
+            Color prev_weight = Color(1.f);
+            Color prev_le     = Color(0.f);
+            Color new_li      = Color(0.f);
+            Ray   bounceRay   = ray; //with depth ==0
+
+            while(bounceRay.depth < m_depth){
+                // update ray and its depth
+                // every event check light(nne)            
+                Intersection its = m_scene->intersect(bounceRay, rng);
+                if(!its) {
+                    // only break when escape the scene
+                    new_li = m_scene->evaluateBackground(bounceRay.direction).value;
+                    break;
+                } 
+                else{
+                    Color new_le      = Color(0.f);
+                    Color new_weight  = Color(1.f);
+
+                    if(nee && bounceRay.depth < m_depth-1){
+                        // nne is recognized as a new bounce, for last bounce, nne is not considered
+                        LightSample light_sample = m_scene->sampleLight(rng);
+
+                        if(!light_sample.light->canBeIntersected()) { 
+                            DirectLightSample direct_light_sample = light_sample.light->sampleDirect(its.position, rng);
+                            bool              light_its           = m_scene->intersect( Ray(its.position, direct_light_sample.wi).normalized(),direct_light_sample.distance,rng);
+
+                            if(!light_its)
+                            {
+                                BsdfEval bsdf_eval                = its.evaluateBsdf(direct_light_sample.wi);
+                                        new_le                  += direct_light_sample.weight * bsdf_eval.value / light_sample.probability;
+                            }
+                        }
+                    }
+
+                    if(its.instance->emission()) new_le += its.evaluateEmission(); //continue tracing
+                    
+                    //sample next ray direction
+                    auto sample_result = its.sampleBsdf(rng);
+                    new_weight         = sample_result.weight;
+
+                    //incremental update
+                    prev_le           += (prev_weight * new_le);
+                    prev_weight       *= new_weight;   
+
+                    if(bounceRay.depth >= m_depth-1) break; //no need to update bounceRay, shortcut
+
+                    //update bounceRay for next iteration, depth +1
+                    bounceRay = Ray(its.position, sample_result.wi, bounceRay.depth+1).normalized();             
+                }
+            }
+            //initial values: prev_le=0, prev_weight=1, new_li=0
+            return prev_le + prev_weight * new_li;
+        }
     }
 
     /// @brief An optional textual representation of this class, which can be useful for debugging. 
